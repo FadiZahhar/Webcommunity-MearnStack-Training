61ea93217e6db58710faf735f06ad2514bde3c1f
charbelmaroundev <charbelmaroun.dev@gmail.com>
2023-06-30 21:49:07 +0300

    backend <> add an express middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const connectDB = require("./config/db.config");
 const app = express();
 connectDB();
 
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to Lebanese Web Community" })
 );
