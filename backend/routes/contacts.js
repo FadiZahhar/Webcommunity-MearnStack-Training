c1f77be43f380a13f01101b774b1f6fe913a2027
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-01 20:10:34 +0300

    finishing the contact model and contact route

--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route   GET api/contacts
 // @desc    Get all users contacts
 // @access Private
-router.get('/', (req,res) => {
-    res.send('Get all users contacts');
+router.get('/', auth, async (req,res) => {
+    try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({date: -1});
+    res.json(contacts);
+    } catch(err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   POST api/contacts
