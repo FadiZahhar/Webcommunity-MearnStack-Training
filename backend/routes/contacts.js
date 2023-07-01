a4c4a7efc254018fb67844f4dccbb4c1224f5c03
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-01 20:24:30 +0300

    finishing the contact routes by adding auth and validation also submitting data to database

--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -29,8 +29,34 @@ router.post('/', (req,res) => {
 // @route   PUT api/contacts:id
 // @desc    Update contact
 // @access Private
-router.put('/:id', (req,res) => {
-    res.send('Update contact');
+router.put('/:id', [auth, [
+    check('name','Name is required')
+    .not()
+    .isEmpty()
+]], async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const { name, email, phone, type} = req.body;
+
+    try {
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+        });
+
+        const contact = await newContact.save();
+
+        res.json(contact);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   DELETE api/contacts:id
