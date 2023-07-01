c2c9562a0b4e313d35d154f301ee2127a5050650
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-01 19:51:58 +0300

    developing our auth middleware

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -10,8 +11,14 @@ const User = require('../models/User');
 // @route   GET api/auth
 // @desc    GET logged in user
 // @access Private
-router.get('/',(req,res) => {
-   res.send('Get Logged in User')
+router.get('/',auth, async (req,res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user)
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 
