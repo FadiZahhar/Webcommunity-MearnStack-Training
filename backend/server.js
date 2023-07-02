2e109d0d6c2fb8dfc189bd00cd6d6d4574219eb8
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-02 22:28:14 +0300

    point 5 was working successfully

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ const app = express()
 
 app.get('/',(req,res) => res.json({msg:'Welcome to Lebanese Web Community'}));
 
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 const PORT = process.env.PORT || 6000;
 
 
