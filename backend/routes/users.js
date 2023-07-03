8402f8bbe5c2b0f6187283b5c4f424fc3bebc192
Mohammad Zaatari <mohamadalzaatari748@gmail.com>
2023-07-03 23:49:45 +0300

    create and respond with JWT

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -39,8 +41,21 @@ router.post('/',[
         user.password = await bcrypt.hash(password, salt);
 
         await user.save();
-        // temperory message to test it out
-        res.send('User saved'); 
+        // creating the payload
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'),{
+            expiresIn: 360000
+        },
+        (err,token) => {
+            if(err) throw err;
+            res.json({token});
+        }
+        );
     }
     catch(err) {
         console.error(err.message);
