4de549a631cc6d0c86c98ba45f980066a327d240
GitJason33 <121455569+GitJason33@users.noreply.github.com>
2023-07-05 22:55:25 +0300

    completed point 8
    hash password, register the user to mongo db

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,41 +1,87 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // import the mongo User model
-const UserModel = require("../models/User.js");
+const User = require("../models/User.js");
+
+// import bcrypt for encrypting the password
+const bcrypt = require("bcryptjs");
 
 // import data validators from express
 const { check, validationResult } = require("express-validator");
 
 let validInputs = [
-  check("name", "Name is required to create an account").not().isEmpty().isLength({ max: 30 }),
+  check("name", "Name is required to create an account")
+    .not()
+    .isEmpty()
+    .isLength({ max: 30 }),
   check("email", "Please include a valid email").isEmail(),
-  check("password", "Please include a password with minimum 8 chars").isLength({ min: 8 })
+  check("password", "Please include a password with minimum 8 chars").isLength({
+    min: 8,
+  }),
 ];
 
 // @route POST api/user
 // @desc  REGISTER a new user
 // @access Public
-router.post("/", validInputs, (req, response) =>{
+router.post("/", validInputs, async (req, response) => {
   // now, get the input validation results and store them in a variable
   const errors = validationResult(req);
 
   // check if there are any errors
-  if(!errors.isEmpty()) 
+  if (!errors.isEmpty())
+    // you can send status code to the client using status() function along with data
     return response.status(400).json({ errors: errors.array() });
 
-  // you can send status code to the client using status() function along with data
-  response.status(200).json({ msg: "User registered successfully!"})
-});
+  // no errors, let's save info in db
+  try {
+    // Step-1
+    const { name, email, password } = req.body;
 
+    // Step-2
+    const existUser = await User.findOne({ email });
+
+    if (existUser)
+      return response.status(400).json({ msg: "User already exists!" });
+
+    // Step-3
+    const user = new User({ name, email, password });
+
+    // Step-4
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    // Step-5
+    await user.save();
+
+    // success message
+    response.status(200).json({ msg: "user registered successfully" });
+
+  } catch (err) {
+    console.error(err);
+    response.status(500).json({ msg: "Something is wrong with the server" });
+  }
+});
 
 /*
  * to use the validators of express:
  * the check is used inside an array given as argument to the router
  * method handler function. depending on how you named the input fields,
- * 
+ *
  * you validate them using pre-built functions like not(), isEmpty(),
- * isEmail(), isLength({ min: a }). 
- * 
-**/
+ * isEmail(), isLength({ min: a, max: b}).
+ **/
 
+/*
+ * summary of steps to save data in db
+ *  1) take user input using req.body
+ *  2)
+ *    a) check if a user with the provided email exists in db
+ *    b) if no user exists, return an error 400
+ *  3) create a new one using the User model
+ *  4) encrypt the password by salting it then hashing it using bcrypt (common security measure)
+ *  5) save the user entry to db
+ *  6) tell the user that it was registered 
+ *
+ * don't forget to try{...}catch(err){...} for potential errors
+ **/
 module.exports = router;
