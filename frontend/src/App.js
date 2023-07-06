3de9402730955ae2b984ad24b6b72cc66484f11e
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-06 20:09:18 +0300

    progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/NavBar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 import './App.css';
 
 const App = () => {
   return (
-    <div className='App'>My App</div>
+    <Router>
+      <Fragment>
+       <Navbar />
+       <div className="container">
+        <Switch>
+          <Route exact path='/' Component={Home} />
+          <Route path='/about' Component={About} />
+        </Switch>
+       </div>
+      </Fragment>
+    </Router>
   );
 };
 
