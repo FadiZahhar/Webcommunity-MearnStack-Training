6ef0eff62fa84bfac88f9da832db6080eea842f6
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-07 00:39:33 +0300

    finishing the contact form

--- frontend/src/context/contact/ContactState.js
+++ frontend/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, {useReducer} from 'react';
-import uuid from 'uuid';
+import {v4 as uuid} from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -44,6 +44,10 @@ import {
     const [state,dispatch] = useReducer(contactReducer, initialState);
 
     // Add Contact
+    const addContact = contact => {
+        contact.id = uuid;
+        dispatch({type: ADD_CONTACT, payload: contact});
+    }
 
     // Delete Contact
 
@@ -59,7 +63,8 @@ import {
 
     return (
         <ContactContext.Provider value={{
-            contacts: state.contacts
+            contacts: state.contacts,
+            addContact
         }}>
             {props.children}
         </ContactContext.Provider>
---
+++ frontend/src/context/contact/contactReducer.js
@@ -0,0 +1,23 @@
+import {
+    GET_CONTACTS,
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+  } from '../types';
+
+const contactReducer = (state, action) => {
+    switch (action.type) {
+        case (ADD_CONTACT):
+            return {
+                ...state,
+                contacts: [...state.contacts,action.payload]
+            }
+        default:
+            return state;
+    }
+}
+  export default contactReducer;
