9624b42a761b9efe3187f3bc8a551c305d66a372
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-08 18:46:43 +0300

    alert state was forgoten to be saved

--- frontend/src/context/alert/AlertState.js
+++ frontend/src/context/alert/AlertState.js
@@ -1,8 +1,11 @@
 import React, { useReducer } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import uuid from 'uuid';
 import AlertContext from './alertContext';
 import alertReducer from './alertReducer';
-import {SET_ALERT, REMOVE_ALERT} from '../types';
+import {
+  SET_ALERT, REMOVE_ALERT
+} from '../types';
+
 
 const AlertState = props => {
   const initialState = [];
@@ -11,7 +14,7 @@ const AlertState = props => {
 
   // Set Alert
   const setAlert = (msg, type, timeout = 5000) => {
-    const id = uuidv4();
+    const id = uuid.v4();
     dispatch({
       type: SET_ALERT,
       payload: {msg, type, id},
