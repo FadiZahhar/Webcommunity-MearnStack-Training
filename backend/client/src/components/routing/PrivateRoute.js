7c62d4aa7952031cb90838a62e485360fffa6da7
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-08 23:22:00 +0300

    trying to deploy after fixed the private route in app.js

--- backend/client/src/components/routing/PrivateRoute.js
+++ backend/client/src/components/routing/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import AuthContext from '../../context/auth/authContext';
-import Spinner from '../layout/Spinner';
+import Spinner from '../../components/layout/Spinner';
 
 const PrivateRoute = ({ component: Component }) => {
   const authContext = useContext(AuthContext);
