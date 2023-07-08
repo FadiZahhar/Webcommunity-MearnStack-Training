10b7b08da7d42b0206fbb1d5a4f273b2c78251e8
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-08 17:36:33 +0300

    preparing the login auth

--- frontend/src/context/auth/AuthState.js
+++ frontend/src/context/auth/AuthState.js
@@ -72,7 +72,27 @@ const AuthState = (props) => {
   }
 
   // Login User
-  const loginUser = () => {console.log('loginuser');}
+  const loginUser = async formData => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    try {
+        const res = await axios.post('/api/auth', formData, config);
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+    }
+    catch(err) {
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: err.response.data.msg
+        });
+    }
+  }
 
   // Logout
   const logoutUser = () => console.log('logoutuser');
