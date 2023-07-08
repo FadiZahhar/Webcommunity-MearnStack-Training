bd90c33a599f6e8edaaf5d686895e0409838a742
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-08 14:20:15 +0300

    starting with the auth context api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,12 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 import './App.css';
 
 const App = () => {
   return (
+    <AuthState>
     <ContactState>
     <BrowserRouter>
       <Fragment>
@@ -22,6 +24,7 @@ const App = () => {
       </Fragment>
     </BrowserRouter>
     </ContactState>
+    </AuthState>
   );
 };
 
