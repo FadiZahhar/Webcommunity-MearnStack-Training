20013a40a442782b751fc3e64dea6f30a69c8297
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-08 21:33:23 +0300

    finishing the delete contact

--- frontend/src/context/contact/ContactState.js
+++ frontend/src/context/contact/ContactState.js
@@ -64,8 +64,19 @@ import {
     }
 
     // Delete Contact
-    const deleteContact = id => {
-        dispatch({ type: DELETE_CONTACT, payload: id});
+    const deleteContact = async id => {
+        try {
+            await axios.delete(`/api/contacts${id}`);
+            dispatch({ 
+                type: DELETE_CONTACT, 
+                payload: id
+            });
+        } catch(err) {
+            dispatch({
+                type: CONTACT_ERROR,
+                payload: err.response.msg
+             });
+        }
     }
 
     // Clear Contacts
--- frontend/src/context/contact/contactReducer.js
+++ frontend/src/context/contact/contactReducer.js
@@ -22,19 +22,19 @@ const contactReducer = (state, action) => {
         case ADD_CONTACT:
             return {
                 ...state,
-                contacts: [...state.contacts,action.payload],
+                contacts: [action.payload,...state.contacts],
                 loading:false
             }
         case UPDATE_CONTACT:
             return {
                 ...state,
-                contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact ),
+                contacts: state.contacts.map(contact => contact._id === action.payload.id ? action.payload : contact ),
                 loading: false
             }
         case DELETE_CONTACT:
             return {
                 ...state,
-                contacts: state.contacts.filter(contact => contact.id !== action.payload),
+                contacts: state.contacts.filter(contact => contact._id !== action.payload),
                 loading: false
             }
         case CLEAR_CONTACTS:
