fac90e02ddddfcc241e135d7815742331f2bd516
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-07-08 14:04:48 +0300

    finishing the csstransition example

--- frontend/src/components/contacts/Contacts.js
+++ frontend/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, {Fragment, useContext } from 'react';
+import { CSSTransition,  TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
@@ -13,14 +14,20 @@ const Contacts = () => {
 
   return (
     <Fragment>
+      <TransitionGroup>
       {filtered !== null 
       ? filtered.map(contact => (
+        <CSSTransition key={contact.id} timeout={500} classNames="item">
         <ContactItem key={contact.id} contact={contact} />
+        </CSSTransition>
       ))
       : contacts.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
+        <CSSTransition key={contact.id} timeout={500} classNames="item">
+        <ContactItem contact={contact} />
+        </CSSTransition>
       ))
       }
+      </TransitionGroup>
     </Fragment>
   );
 }
