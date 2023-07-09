96fa7714029ed13b27c54739403393573b9bfda1
Mo-Ali11 <born2code2003@gmail.com>
2023-07-09 11:54:38 +0300

    connecting to the database

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-// import Routes:
+/*importing routes */
 import usersRoutes from './routes/users.js';
 import contactRoutes from './routes/contact.js';
 import authRoutes from './routes/auth.js';
 
+/*configuration*/
+dotenv.config();
 const app = express();
-app.get('/', (req, res) => res.json({ msg: 'testing everything is good' }));
+app.use(express.json());
+
+/*routes*/
 app.use('/api/users', usersRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/contacts', contactRoutes);
 
+/*mongoose setup*/
 const PORT = process.env.PORT || 9000;
-
-app.listen(PORT, () => console.log(`Server started on localhost:${PORT}`));
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server started on localhost:${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
