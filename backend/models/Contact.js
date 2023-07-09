08e6030f7da5b03040c485b3031ea0f0798ae061
Mo-Ali11 <born2code2003@gmail.com>
2023-07-09 16:36:01 +0300

    added contact model and get contacts route

---
+++ backend/models/Contact.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: 'personal',
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Contact = mongoose.model('Contact', ContactSchema);
+export default Contact;
--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import auth from '../middleware/auth.js';
+import { check, validationResult } from 'express-validator';
+import User from '../models/User.js';
+import Contact from '../models/Contact.js';
 const router = express.Router();
 
 // @route   GET api/contacts
 // @desc    Get all users contacts
 // @access Private
-router.get('/', (req, res) => {
-  res.send('Get all users contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST api/contacts
