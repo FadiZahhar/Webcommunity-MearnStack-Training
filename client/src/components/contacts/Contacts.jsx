34c420d337718e1f1caeef36f92bd2a07de41fac
GitJason33 <121455569+GitJason33@users.noreply.github.com>
2023-08-04 11:37:50 +0300

    completed frontend part 1

--- client/src/components/contacts/Contacts.jsx
+++ client/src/components/contacts/Contacts.jsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
@@ -12,14 +14,20 @@ const Contact = () => {
 
   return (
     <>
-      {filtered !== null
-        ? filtered.map(contact => (
-          <ContactItem key={contact.id} contact={contact} />
-        ))
-        : contacts.map(contact => (
-          <ContactItem key={contact.id} contact={contact} />
-        ))
-      }
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map(contact => (
+            <CSSTransition key={contact.id} timeout={500} classNames="item">
+              <ContactItem contact={contact} />
+            </CSSTransition>
+          ))
+          : contacts.map(contact => (
+            <CSSTransition key={contact.id} timeout={500} classNames="item">
+              <ContactItem contact={contact} />
+            </CSSTransition>
+          ))
+        }
+      </TransitionGroup>
     </>
   )
 }
