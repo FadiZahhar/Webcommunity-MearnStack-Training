6cee32440115e6d2b1353e300af0f5c131bbd3f8
tonicharbel <tonicharbel64@gmail.com>
2023-08-09 10:51:43 +0300

    upload more code

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/NavBar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Fragment>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route path="/about" Component={About} />
+        </Routes>
+      </div>
+    </Fragment>
+    </BrowserRouter>
+  );
+};
+
+export default App;
