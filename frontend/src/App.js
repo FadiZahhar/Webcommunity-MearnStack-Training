69d5c651be1913f08d1554a0b300665e97497d19
Jessica <bassiljessica18@gmail.com>
2023-08-15 15:10:38 +0300

    Building NavBar and Main Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 import "./App.css";
+import CNavbar from "./components/layout/CNavBar";
 
 const App = () => {
-  return <h1>Front end ready</h1>;
+  return (
+    <BrowserRouter>
+      <Fragment>
+        <CNavbar />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" Component={Home} />
+            <Route path="/about" Component={About} />
+          </Routes>
+        </div>
+      </Fragment>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/layout/CNavBar.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Navbar,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarBrand,
+} from "reactstrap";
+
+const CNavbar = ({ title, icon }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Navbar color="dark" dark expand="md">
+      <NavbarBrand href="/">
+        <i className={icon}></i> {title}
+      </NavbarBrand>
+      <NavbarToggler
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink className="text-light" href="/about">
+              About
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+CNavbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+CNavbar.defaultProps = {
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt",
+};
+
+export default CNavbar;
